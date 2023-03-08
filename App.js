48b37667cefd90c1263cd4640f5d80823ed884b2
yuhyeon99 <jujini31@gmail.com>
2023-03-08 14:27:25 +0900

    init

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello! I  made a RN App!</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
 
+// StyleSheet.create를 사용해서 객체를 생성하면 스타일에 관한 자동완성기능이 작동한다
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +18,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text : {
+    fontSize:28,
+    color:'red'
+  }
 });
